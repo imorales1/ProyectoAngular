da6c2cc9e92446fe494fbb59ce72cc33996e0727
ISMAR E. Morales <imorales@sandiego.com.gt>
2022-03-09 09:01:09 -0600

    Se Agrego el servicio Web

---
+++ FBTarjeta/FBTarjeta/AplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using FBTarjeta.Models;
+
+namespace FBTarjeta
+{
+    public class AplicationDbContext: DbContext
+    {
+        public DbSet<TarjetaCredito> TarjetaCredito { get; set; }
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ FBTarjeta/FBTarjeta/Models/TarjetaCredito.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FBTarjeta.Models
+{
+    public class TarjetaCredito
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Titular { get; set; }
+        [Required]
+        public string NumeroTarjeta { get; set; }
+        [Required]
+        public string FechaExpiracion { get; set; }
+        [Required]
+        public string cvv { get; set; }
+
+
+    }
+}
